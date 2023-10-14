41df5c3c2c50bf393fd90611ed54a22939e9d369
M.L.Akila Liyanage <mlakilaliyanage@gmail.com>
2023-10-14 23:38:51 +0530

    feat(jira): added jira int

--- scripts/other.js
+++ scripts/other.js
@@ -7,6 +7,9 @@ const configFormButton = document.getElementById("configFormButton");
 const workspaceIdInput = document.getElementById("clokifyWorkspaceIdInput");
 const projectIdInput = document.getElementById("clokifyProjectIdInput");
 const PATInput = document.getElementById("clokifyPATInput");
+const jiraEmailInput = document.getElementById("jiraEmailInput");
+const jiraPATInput = document.getElementById("jiraPATInput");
+const jiraTaskIdInput = document.getElementById("jiraTaskIdInput");
 
 configFormButton.addEventListener("click", (e) => {
   e.preventDefault();
@@ -15,9 +18,27 @@ configFormButton.addEventListener("click", (e) => {
     workspaceId: workspaceIdInput.value,
     projectId: projectIdInput.value,
     PAT: PATInput.value,
+    jiraEmail: jiraEmailInput.value,
+    jiraPAT: jiraPATInput.value,
+    jiraTaskId: jiraTaskIdInput.value,
   };
 
   chrome.storage.sync.set(configurations, () => {
+    console.log(configurations);
     alert("Configuration saved!");
   });
 });
+
+window.onload = () => {
+  chrome.storage.sync.get(
+    ["workspaceId", "projectId", "PAT", "jiraEmail", "jiraPAT", "jiraTaskId"],
+    (configurations) => {
+      workspaceIdInput.value = configurations.workspaceId;
+      projectIdInput.value = configurations.projectId;
+      PATInput.value = configurations.PAT;
+      jiraEmailInput.value = configurations.jiraEmail;
+      jiraPATInput.value = configurations.jiraPAT;
+      jiraTaskIdInput.value = configurations.jiraTaskId;
+    },
+  );
+};
